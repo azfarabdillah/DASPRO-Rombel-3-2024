e65e4c6e3fe41f98a2ebd5966b1d52205cfe4084
azfarabdillah <rifasyah4@gmail.com>
2024-10-10 21:19:17 +0700

    Update 1-G.cpp

---
+++ Tugas TLX/2405100080_RIFASYAH AZFAR ABDILLAH/1-G.cpp
@@ -0,0 +1,9 @@
+Soal
+Apakah keluaran dari potongan program berikut?
+std::cout << "semangat " << (9 * 5) << std::endl;
+
+Jawaban
+semangat 45
+
+Penjelasan
+  kata semangat harus berada di tengah "" agar bisa menyatakan semangat, dan hasil dari perkalian 9 * 5 akan menyatakan 45 
