b11b7c045f7a6638c4d3c899e7c8bf0c63bc2783
azfarabdillah <rifasyah4@gmail.com>
2024-10-10 21:29:24 +0700

    Update 1-G.cpp

---
+++ Tugas TLX/2405100080_RIFASYAH AZFAR ABDILLAH/1-G.cpp
@@ -0,0 +1,9 @@
+Soal
+Apakah keluaran dari potongan program berikut?
+std::cout << "semangat " << (9 * 5) << std::endl;
+
+Jawaban
+semangat 45
+
+Penjelasan
+kata semangat yang berada di dalam "" sebelum std::cout akan memunculkan kata semangat, dan hasil dari 9 * 5 adalah 45. Jadi keluaran program tersebut adalah semangat 45
